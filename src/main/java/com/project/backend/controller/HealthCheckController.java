31da299258d6ddff54b9fb598c195a5496b90866
ksj <105255745+ccoodd0303@users.noreply.github.com>
2026-05-27 12:58:40 +0900

    서버 확인용 api

--- src/main/java/com/project/backend/controller/HealthCheckController.java
+++ src/main/java/com/project/backend/controller/HealthCheckController.java
@@ -1,4 +1,14 @@
 package com.project.backend.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HealthCheckController {
+    
+    @GetMapping("/")
+    public ResponseEntity<String> healthCheck() {
+        return ResponseEntity.ok("Server is running!");
+    }
 }
